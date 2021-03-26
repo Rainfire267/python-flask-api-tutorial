eae9b173faa2179a697a4ab7254c1df5c1209e4d
Rainfire267 <ejimenez267@gmail.com>
2021-03-26 22:01:16 +0000

    1st version endpoints

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+#acá agrego variable global
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+
+def hello_world():
+    return (jsonify(todos))
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data #request es lo que recibo; .data es lo que tengo en el body y accedo a la propiedad donde esta la info
+    print("Incoming request with the following body", request_body)
+    #
+    decoded_object = json.loads(request.data) #loads convierte json a formato que python entiende (a un diccionario)
+    todos.append(decoded_object)#agregar la tarea nueva usando append
+    return (jsonify(todos)) #
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+    
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 3245, debug =  True)
