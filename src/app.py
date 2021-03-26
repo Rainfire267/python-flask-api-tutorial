53236093d9906df56a9aa4ce1d3bfd6b018ce2b1
Rainfire267 <ejimenez267@gmail.com>
2021-03-26 22:48:45 +0000

    2nd version

--- src/app.py
+++ src/app.py
@@ -17,15 +17,15 @@ def hello_world():
 def add_new_todo():
     request_body = request.data #request es lo que recibo; .data es lo que tengo en el body y accedo a la propiedad donde esta la info
     print("Incoming request with the following body", request_body)
-    #
     decoded_object = json.loads(request.data) #loads convierte json a formato que python entiende (a un diccionario)
     todos.append(decoded_object)#agregar la tarea nueva usando append
-    return (jsonify(todos)) #
+    return (jsonify(todos)) 
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete: ",position)
-    return 'something'
+    todos.pop(position)
+    return jsonify(todos)#llamo a mi variable global como lista para eliminar el índice que el usuario indique
     
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 3245, debug =  True)
